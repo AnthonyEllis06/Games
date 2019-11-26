1b4e39797c35628c73c545a28a5a141dacebff86
Anthony <anthony.ellis06@gmail.com>
2019-11-25 21:15:56 -0500

    added game listener and added to game interface to get a better idea of how to make this an even based project also currently can add checker to the same tile it is at.

--- src/GameUtil/GameInterface.java
+++ src/GameUtil/GameInterface.java
@@ -1,6 +1,10 @@
 package GameUtil;
 
 public interface GameInterface extends GameLogicListener{
+
+    //The idea is to have the play method in this game interface make the games al run similar and smooth.
+    //It works by continuously making players take turns
+    // needs notify to be used to allow players for use strats
     default void play(){
         int numPlayers = getNumPlayers();
         Player curr = getCurrPlayer();
@@ -16,10 +20,17 @@ public interface GameInterface extends GameLogicListener{
         }
         gameOver();
     }
+    //returns current player
     Player getCurrPlayer();
-    void addPlayer(Player newPlayer);
+    //used in play to add nwew players
+    void addPlayer(Player newPlayer)
+    //sets the turn for the next player;
     void nextPlayerTurn();
+    //checks if game is won in win condition.
     Boolean gameWon();
+    //returns the total amount of players
     int getNumPlayers();
+    //used to see if game is over and also where you can
+    //do cals for a custom game over.
     Boolean gameOver();
 }
